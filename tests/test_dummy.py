5173529a95af82d030916ad9ae1b7f59d3dd6c29
Kevin <kevinpietrzyk@gmail.com>
2017-05-10 18:43:25 +0200

    zmiana

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -10,11 +10,35 @@ Run with PYTHONPATH=. python tests/test_dummy.py
 class TestDummy(unittest.TestCase):
 
     def test_fun(self):
-        weekday = calculate(2001, 1, 3)
-        self.assertEqual(weekday, 2005)
+        weekday = calculate(2017, 5, 10)
+        self.assertEqual(weekday, 2)
 
-        retcode = main(("--year", "2001", "--month", "1", "--day", "3"))
-        self.assertEqual(retcode, 0)
+        weekday = calculate(2020, 2, 29)
+        self.assertEqual(weekday, 6)
+
+        weekday = calculate(2001, -1, 3)
+        self.assertEqual(weekday, -1)
+
+        weekday = calculate(2013, 13, 13)
+        self.assertEqual(weekday, -1)
+
+        weekday = calculate(2013, 'a', 13)
+        self.assertEqual(weekday, -1)
+
+        retcode3 = main(("--year", "2001", "--month", "1", "--day", "3", "abc", "sgsg"))
+        self.assertEqual(retcode3, -1)
+
+        retcode4 = main(("--year", "2020", "--month", "2", "--day", "29"))
+        self.assertEqual(retcode4, 6)
+
+        retcode5 = main(("--year", "1990", "--month", "2", "--day", "29"))
+        self.assertEqual(retcode5, -1)
+
+        retcode6 = main(("--year", "2017", "--month", "4", "--day", "31"))
+        self.assertEqual(retcode6, -1)
+
+        retcode7 = main(("--year", "2017", "--month", "5", "--day", "10"))
+        self.assertEqual(retcode7, 2)
 
 
 if __name__ == '__main__':
